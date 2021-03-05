6a07ded3683c2d8ef74c68a8e1c4dfc16f83801e
worgflores <worgflores@gmail.com>
2021-03-05 22:07:41 +0800

    services: updated

--- services/modules/rptis/rameses-gov-etracs-rptis-landtax-services.jar/rptis/landtax/facts/RPTLedgerFact.groovy
+++ services/modules/rptis/rameses-gov-etracs-rptis-landtax-services.jar/rptis/landtax/facts/RPTLedgerFact.groovy
@@ -17,6 +17,9 @@ public class RPTLedgerFact
     String barangayid
     String barangay
     String rputype
+    String tdno
+    String prevtdno
+    String txntype
 
     //data reference
     def entity    
@@ -32,12 +35,15 @@ public class RPTLedgerFact
         this.firstqtrpaidontime     = (ledger.firstqtrpaidontime == 1 || (ledger.firstqtrpaidontime ? true : false ))
         this.qtrlypaymentpaidontime = (ledger.qtrlypaymentpaidontime == 1 || (ledger.qtrlypaymentpaidontime ? true : false ))
         this.undercompromise        = (ledger.undercompromise == 1 || (ledger.undercompromise ? true : false ))
-        this.parentlguid                  = ledger.parentlguid
+        this.parentlguid            = ledger.parentlguid
         this.lguid                  = ledger.lguid
         this.barangayid             = ledger.barangayid
         this.barangay               = ledger.barangay
         this.rputype                = ledger.rputype
+        this.tdno                   = ledger.tdno
+        this.prevtdno               = ledger.prevtdno
         this.missedpayment          = ledger.missedpayment
+        this.txntype                = ledger.txntype?.objid
     }
 
 }
